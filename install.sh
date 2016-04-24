a28917284e834973cb846544ab9ff67ba3b5b84a
Steeve Gaudreault-Lambert <steevegl.dev@gmail.com>
2016-04-24 13:00:04 -0400

    fix  symbolic link

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 DIR=/home/`whoami`/.vhost
 if [[ ! -d $DIR ]] ; then
     mkdir -p ~/.vhost
+    sudo ln -sv $DIR /root/.vhost
 fi
 
 cp -vR share ~/.vhost
 sudo cp -v vhost.py /usr/bin/vhost
-sudo ln -sv $DIR /root/.vhost
 
 if [ ! -f ~/.vhost/vhost.conf ]; then
     cp -v vhost.conf ~/.vhost/vhost.conf
